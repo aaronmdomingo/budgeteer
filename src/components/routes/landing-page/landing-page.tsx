9ad892bcc9d51a59c509424ee3efaa2170c73f73
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-01 18:41:56 -0800

    fixed button size and container sizing

--- src/components/routes/landing-page/landing-page.tsx
+++ src/components/routes/landing-page/landing-page.tsx
@@ -17,12 +17,14 @@ const LandingPage = ({props}: any) => {
             { isClicked 
             ? ''
             :  
-            <div className="landing__page_button" onClick={clickHandler}>
-                <span>Get Started</span>
-                <svg>
-                    <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
-                    <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
-                </svg>
+            <div className="landing__page_actions" onClick={clickHandler}>
+                <div className="landing__page_button">
+                    <span>Get Started</span>
+                    <svg>
+                        <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                        <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                    </svg>
+                </div>
             </div> 
             }
             <CSSTransition 
@@ -34,14 +36,14 @@ const LandingPage = ({props}: any) => {
                 {
                     state => (
                         <div className="landing__page_login">
-                            <div className="landing__page_button landing__page_button-sub">
+                            <div className="landing__page_button">
                                 <span>Register</span>
                                 <svg>
                                     <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
                                     <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
                                 </svg>
                             </div> 
-                            <div className="landing__page_button landing__page_button-sub">
+                            <div className="landing__page_button">
                                 <span>Log in</span>
                                 <svg>
                                     <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
