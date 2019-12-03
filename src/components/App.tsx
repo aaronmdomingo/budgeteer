b8ac101f9318b131cebffd69957656ba46e7f721
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-02 16:37:19 -0800

    created the dashboard component connected withRouter to render the url dynamically

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 import LandingPage from './routes/landing-page/landing-page';
+import Dashboard from './routes/dashboard/dashboard';
 
 const App: React.FC = () => {
+  const [currentUser, setCurrentUser] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logInUser = (user: string) => {
+    setCurrentUser(user);
+    setIsLoggedIn(true);
+  }
+
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
-         <LandingPage />
+         <LandingPage logInUser={logInUser}/>
+        </Route>
+        <Route path='/dashboard/:user'>
+          <Dashboard currentUser={currentUser} />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/routes/dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+
+const Dashboard = (props: any) => {
+    return (
+        <h1>Test</h1>
+    )
+}
+
+export default withRouter(Dashboard);
