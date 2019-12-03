2ff56c22231c95a37c09743565018281e00d21fc
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-02 19:18:01 -0800

    Added a new method to get current month passed into dashboard component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,20 +10,34 @@ import Dashboard from './routes/dashboard/dashboard';
 const App: React.FC = () => {
   const [currentUser, setCurrentUser] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentMonth, setCurrentMonth ] = useState('');
+
+  useEffect(() => {
+    const date = new Date();
+      const month = date.getMonth();
+      const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+      const monthName = monthsArr[month];
+    setCurrentMonth(monthName);
+  }, []);
 
   const logInUser = (user: string) => {
     setCurrentUser(user);
     setIsLoggedIn(true);
   }
 
+  const setMonth = (month: string) => {
+    setCurrentMonth(month);
+  }
+
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
-         <LandingPage logInUser={logInUser}/>
+         <LandingPage logInUser={logInUser} currentMonth={currentMonth}/>
         </Route>
-        <Route path='/dashboard/:user'>
-          <Dashboard currentUser={currentUser}/>
+        <Route path='/dashboard/:user/:monthName'>
+          <Dashboard isLoggedIn={isLoggedIn} currentUser={currentUser} setMonth={setMonth} currentMonth={currentMonth}/>
         </Route>
       </Switch>
     </Router>
