93e94a919f6040d3ef520c479a5d68301a67c451
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-02 17:39:45 -0800

    added method connecting to dashboard, and removed unecessary code

--- src/components/routes/dashboard/dashboard.tsx
+++ src/components/routes/dashboard/dashboard.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import './_dashboard.scss';
 
 
 const Dashboard = (props: any) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June',
+                     'July', 'August', 'September', 'October', 'November', 'December'];
+    const [showSideBar, setShowSideBar] = useState(false);
+
+    const sideBarHandler = () => {
+        setShowSideBar(!showSideBar);
+    }
+
+
     return (
-        <h1>Test</h1>
+        <div className="dashboard">
+            <div className="dashboard__header">
+                <div className="dashboard__header_menu">
+                <i className="fas fa-bars" onClick={sideBarHandler} ></i>
+                </div>
+                <div className="dashboard__header_user_info">
+
+                </div>
+            </div>
+            <div className="dashboard__main">
+
+            </div>
+            <CSSTransition 
+                in={showSideBar} 
+                appear={showSideBar}
+                timeout={800} 
+                classNames="fade"
+                unmountOnExit>
+                {/* {
+                    state => (
+                       <div className="dashboard__sidebar">
+                           .dashboard__side
+                       </div>
+                    )
+                } */}
+                <div className="dashboard__sidebar">
+                           .dashboard__side
+                       </div>
+            </CSSTransition>
+        </div>
     )
 }
 
