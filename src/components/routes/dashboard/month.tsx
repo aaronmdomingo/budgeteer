0718fc15d0e24a9ef6f5c34894404b3f7a68479a
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-04 20:49:05 -0800

    imported useparams and utlized url for when making the fetch call

--- src/components/routes/dashboard/month.tsx
+++ src/components/routes/dashboard/month.tsx
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
 import { useCookies } from 'react-cookie';
 
 const Month = (props: any) => {
-    const { currentUser, currentMonth, setMonth } = useContext(UserContext);
+    const { currentUser } = useContext(UserContext);
     const [ userCookie ] = useCookies(['current-user']);
+    const { monthName } = useParams();
 
     const goToMonth = () => {
-        setMonth(props.month);
         props.history.push(`/dashboard/${currentUser || userCookie['current-user'] }/${props.month}`);
         props.sideBarHandler();
     }
 
     return (
-    <div className={`dashboard__sidebar_months_val ${props.month === currentMonth ? 'current' : ''}`} onClick={goToMonth}>
+    <div className={`dashboard__sidebar_months_val ${props.month === monthName ? 'current' : ''}`} onClick={goToMonth}>
         {props.month}
     </div>
     )
