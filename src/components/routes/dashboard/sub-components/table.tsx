0fe8d691b488148c1457f92f1e2bf1ed81ee971a
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-09 20:32:20 -0800

    Added get fetch method to get current month info from backend

--- src/components/routes/dashboard/sub-components/table.tsx
+++ src/components/routes/dashboard/sub-components/table.tsx
@@ -3,13 +3,32 @@ import Expense from './expense';
 import { animateScroll } from 'react-scroll';
 
 const Table = (props: any) => {
-    const { monthName, expenseArr, expenseFormHandler, isLoading, deleteExpense, updateExpense } = props;
+    const { monthName, user, expenseArr, expenseFormHandler, isLoading, deleteExpense, updateExpense } = props;
     const [ currentTab, setCurrentTab ] = useState(0);
+    const [ budget, setBudget ] = useState(0);
+    const [ expense, setExpense ] = useState(0);
+    const [ status, setStatus ] = useState('');
 
     useEffect(() => {
         setCurrentTab(0);
         animateScroll.scrollToBottom({ duration: 1500, containerId: 'dashboard--table' });
+        fetchBudget();
     }, [isLoading, expenseArr])
+
+    const fetchBudget = () => {
+        fetch(`/api/month/${user}/${monthName}`)
+            .then(res => res.json())
+            .then(res => {
+                let totalExpense = 0;
+                expenseArr.forEach((e :any) => totalExpense += e.value)
+                setExpense(totalExpense);
+                setBudget(res.current_budget - totalExpense);
+            })
+            .catch(err => alert(err));
+    }
+
+    const changeStatus = (num: number) => {
+    }
 
     const toggleView = (id: number) => {
         currentTab === id ? setCurrentTab(0) : setCurrentTab(id);
@@ -19,7 +38,28 @@ const Table = (props: any) => {
         <div className="dashboard__table">
             <div className="dashboard__table_container">
                 <div className="dashboard__table_container-header">
+                    <div className="month">
                     { monthName }
+                    </div>
+                    <div className="budget">
+                        <div className="text">
+                            Available Budget:
+                        </div>
+                        <div className="value">
+                            { budget }
+                        </div>
+                    </div>
+                    <div className="total-expense">
+                        <div className="text">
+                            Total Expenses:
+                        </div>
+                        <div className="value">
+                            {  expense }
+                        </div>
+                    </div>
+                    <div className="status">
+                        You're doing a good job so far, keep at it!
+                    </div>
                 </div>
                 <div className="dashboard__table_container-body" id="dashboard--table">
                     {
