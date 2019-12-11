9c8baf9854ec488d10af549642959c16190fcc1b
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-10 21:23:07 -0800

    defined new dashboard sub component and with proper click handlers

---
+++ src/components/routes/dashboard/sub-components/modal.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+
+const Modal = (props: any) => {
+    const { setShowModal, monthName, budget } = props;
+    const [ updatedBudget, setUpdatedBudget ] = useState(budget);
+
+    const handleChange = (event: any) => {
+        setUpdatedBudget(event.target.value);
+    }
+
+    return (
+        <div className="dashboard__modal">
+            <form className="dashboard__modal_container">
+                <div className="text">
+                    Current budget for the month of:
+                </div>
+                <div className="month">
+                    {monthName}
+                </div>
+                <div className="value">
+                    <input type="number" min="0" className="input" value={updatedBudget} onChange={handleChange} />
+                </div>
+                <div className="button">
+                    <div className="button-table button-update">
+                        <button type="submit">Update</button>
+                    </div>
+                    <div className="button-table button-cancel" onClick={() => setShowModal(false)}>
+                        Cancel
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Modal;
