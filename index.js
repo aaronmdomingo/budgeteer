4ee2a43d6238ee27f6acd5514814b2a200381e32
Aaron Domingo <aaronmdomingo@gmail.com>
2019-12-16 00:54:13 -0800

    removed body-parser from dependencies

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {  
